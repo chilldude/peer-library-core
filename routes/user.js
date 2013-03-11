cb5a9800d6d0d24f499517c6b437edf01838ea18
Rodrigo <rodrigo@jesus.(none)>
2013-03-10 20:43:50 -0700

    merge fixes

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,16 @@
-/*
- * GET users listing.
- */
+var models = require('../schemas/models');
 
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
 
 exports.profile = function(req, res){
-  res.render('profile', {'title': 'Peer Library'});
+  models.User.findById(req.user._id).populate('institution').exec(function (err, user) {
+    res.render('profile', {
+      title: 'Peer Library',
+      name: user.name_first + ' ' + user.name_last,
+      title: user.title,
+      institution: user.institution.name
+    });
+  });
 };
